46254cf62946270a1fa8639b8e5d3047d6e9947d
faritreascodev <areasco1306nnca@gmail.com>
2025-06-14 17:04:36 -0500

    Carga inicial de los ejercicios en clase - DW

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import './estilos.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+          <h1>Farit Reasco</h1>
+          <h2>Estudiante de Desarrollo de Software</h2>
+      </div>
+      <div className="App">
+          <p>En esta clase veremos los fundamentos de React, incluyendo componentes, props y estado.</p>
+      </div>
+    </>
   );
 }
 
